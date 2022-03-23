ae4d93d2a92634ac3e988382488f9efd3129b840
Frederick Morin <frederickmorin18@gmail.com>
2022-03-23 19:43:55 -0400

    Add support for fragment shorthand

--- .babelrc.js
+++ .babelrc.js
@@ -11,6 +11,15 @@ module.exports = {
     ],
   ],
   plugins: [
+    [
+      '@babel/plugin-transform-react-jsx',
+      {
+        // this config enables using the fragment shorthand: `<> </>`
+        // without having to `import React from 'react'`
+        runtime: 'automatic',
+        importSource: '@emotion/react',
+      },
+    ],
     '@emotion/babel-plugin',
     'babel-plugin-twin',
     'babel-plugin-macros',
