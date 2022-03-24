0620bcfa8ba98c87a19d438890c9fd6e37456953
Frederick Morin <frederickmorin18@gmail.com>
2022-03-24 19:25:40 -0400

    Allow require statements in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,13 +23,19 @@ module.exports = {
 
     /**
      * Allow explicit usage of the `any` type. This provides an escape hatch, but should be used with caution.
-     * @see https://github.com/typescript-eslint/typescript-eslint/blob/HEAD/packages/eslint-plugin/docs/rules/no-explicit-any.md
+     * @see https://typescript-eslint.io/rules/no-explicit-any
      */
     '@typescript-eslint/no-explicit-any': 0,
 
     /**
+     * Allow `require` statements. Should be used only in js config files eg. `tailwind.config.js`.
+     * @see https://typescript-eslint.io/rules/no-var-requires/
+     */
+    '@typescript-eslint/no-var-requires': 0,
+
+    /**
      * Requires descriptive comment when using `@ts-<directive>`.
-     * @see https://github.com/typescript-eslint/typescript-eslint/blob/HEAD/packages/eslint-plugin/docs/rules/ban-ts-comment.md
+     * @see https://typescript-eslint.io/rules/ban-ts-comment
      */
     '@typescript-eslint/ban-ts-comment': [
       'error',
