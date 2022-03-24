ab63e368ef231c486518d1fc8ed72a769fbff385
Frederick Morin <frederickmorin18@gmail.com>
2022-03-24 19:21:18 -0400

    Add all colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   theme: {
-    extend: {
-      colors: {
-        electric: '#db00ff',
-        ribbon: '#0047ff',
-      },
+    colors: {
+      ...colors,
     },
   },
   plugins: [],
